8cf9a541fdb92198d182636dfa084b49cbe03426
Kasse-Dembele <kasse.dembele@gmail.com>
2023-02-26 17:41:16 +0100

    feat(room): CRUD operation for room

--- study_bud/core/urls.py
+++ study_bud/core/urls.py
@@ -6,4 +6,7 @@ app_name = "core"
 urlpatterns = [
     path("", views.home, name="home"),
     path("room/<int:pk>", views.room, name="room"),
+    path("room/new", views.create_room, name="create-room"),
+    path("room/<int:pk>/", views.edit_room, name="edit-room"),
+    path("room/<int:pk>/delete/", views.delete_room, name="delete-room"),
 ]
--- study_bud/core/views.py
+++ study_bud/core/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
-
-rooms = [
-    {"id": 1, "name": "Lets Learn python!"},
-    {"id": 2, "name": "Design with me"},
-    {"id": 3, "name": "Frontend developpers"},
-]
 
 
 def home(request):
@@ -20,3 +15,35 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {"room": room}
     return render(request, "core/room.html", context)
+
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("core:home")
+
+    context = {"form": form}
+    return render(request, "core/room_form.html", context)
+
+
+def edit_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("core:home")
+    context = {"form": form}
+    return render(request, "core/room_form.html", context)
+
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect("core:home")
+    return render(request, "core/delete.html", {"obj": room})
